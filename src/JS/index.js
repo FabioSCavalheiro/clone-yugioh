613578c9c1266200a53ce34a5ba9a585a55038ca
FabioSCavalheiro <fabioscavalheiro@gmail.com>
2025-01-28 10:42:55 -0300

    Add files via upload

--- src/JS/index.js
+++ src/JS/index.js
@@ -18,22 +18,39 @@ let cartaoAtual = 0;
 
 //console.log(EX) = para aparecer no console do navegador - para ver os objetos e classes do produto htlm que está mechendo
 
+//ultima live
+//forEach serve para passar por cada item da lista eventListener é para 1 unico item 
+cartoes.forEach((cartao) => {
+  cartao.addEventListener("click", function () {
+    const cartaVirada = cartao.querySelector(".carta-virada")
+
+    //virar o cartão 
+    cartao.classList.toggle("virar");
+    //mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+
+  });
+});
+
 //passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
 
 btnAvancar.addEventListener("click", function () {
-    //são 7 cartões mas a contagem inicial no zero, então precisa por -1 para o codigo ler do 0 ao 6 não do 0 ao 7 (return - faz o código parar)
-    if (cartaoAtual === cartoes.length - 1) return;
-    //passo 4 - buscar o cartão que esta selecionado e esconder
-    //cada palavra nova a letra é maiuscula \/ - lembrar do . na classe 
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+  //são 7 cartões mas a contagem inicial no zero, então precisa por -1 para o codigo ler do 0 ao 6 não do 0 ao 7 (return - faz o código parar)
+  if (cartaoAtual === cartoes.length - 1) return;
+  //passo 4 - buscar o cartão que esta selecionado e esconder
+  //cada palavra nova a letra é maiuscula \/ - lembrar do . na classe 
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
 
-    // passo 3 - fazer aparecer o próximo cartão da lista 
-    //função para deixar alterar o cartao principal e ofuscar o resto\/ alterando a classe selecionado do 1 para o 2 e assim em diante
-    //++ para subir/incrementar/proximo
-    cartaoAtual++;
-    //esse classlist dará erro, pois não existe cartoes além do setimo
-    cartoes[cartaoAtual].classList.add("selecionado");
+  // passo 3 - fazer aparecer o próximo cartão da lista 
+  //função para deixar alterar o cartao principal e ofuscar o resto\/ alterando a classe selecionado do 1 para o 2 e assim em diante
+  //++ para subir/incrementar/proximo
+  cartaoAtual++;
+  //esse classlist dará erro, pois não existe cartoes além do setimo
+  cartoes[cartaoAtual].classList.add("selecionado");
 });
 
 /*
@@ -48,13 +65,13 @@ const btnVoltar = document.getElementById("btn-voltar");
 
 btnVoltar.addEventListener("click", function () {
   //se chegar na posição 0 significa que é o primeiro
-    if (cartaoAtual === 0) return;
-  
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+  if (cartaoAtual === 0) return;
 
-    //-- para diminuir/anterior/decrementar
-    cartaoAtual--;
-   
-    cartoes[cartaoAtual].classList.add("selecionado");
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+
+  //-- para diminuir/anterior/decrementar
+  cartaoAtual--;
+
+  cartoes[cartaoAtual].classList.add("selecionado");
 });
